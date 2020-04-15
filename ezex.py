7ac4ccbf61abcec2d93e593f34ab3faa61d59c30
ez <evgeny.zenkin@gmail.com>
2020-04-15 18:17:59 +0300

    use template to generate html page

--- ezex.py
+++ ezex.py
@@ -1,31 +1,5 @@
 import flask
-import random
-
-
-max_num = 20
-
-
-def gen_one_ex():
-    a, b = random.randrange(1, max_num), random.randrange(1, max_num)
-    if a < b:
-        a, b = b, a
-    return ' '.join([str(a), random.choice(['+', '-']), str(b), "="])
-
-
-def gen_ex_str(num):
-    res = '<tr>'
-    for i in range(num):
-        res += '<td style="padding: 6px">' + gen_one_ex() + '</td>'
-    res += '</tr>'
-    return res
-
-
-def gen_table(row, col):
-    res = '<!DOCTYPE html><font style="font-size:24px"><b><table style="width:100%">'
-    for i in range(row):
-        res += gen_ex_str(col)
-    res += '</table></b>'
-    return res
+import generate
 
 
 app = flask.Flask(__name__)
@@ -33,8 +7,9 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def home():
-    return gen_table(25, 4)
+    t = flask.Markup(generate.table(25, 4))
+    return flask.render_template('page.html', table=t)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=9999)
